a88203c3fd5146fc815f25e890a607e5c2b06893
Min1870 <minkhantzaw.peti@gmail.com>
2023-08-15 23:36:49 +0630

    addProfOver

---
+++ src/components/Sidebar/SideBar.jsx
@@ -0,0 +1,315 @@
+import React, { useState } from "react";
+import { MdSpaceDashboard } from "react-icons/md";
+import { AiFillSetting, AiFillDashboard, AiFillCalendar } from "react-icons/ai";
+import { IoIosMenu } from "react-icons/io";
+import { HiUsers } from "react-icons/hi";
+import { FaCoins, FaLayerGroup } from "react-icons/fa";
+import {
+  BsHouseDoorFill,
+  BsPencilSquare,
+  BsBoxArrowRight,
+} from "react-icons/bs";
+import "./sidebar.css";
+import { Accordion, ScrollArea } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Link, NavLink } from "react-router-dom";
+
+import overview from "../../../public/overview.svg";
+import sale from "../../../public/sale.svg";
+import iventory from "../../../public/iventory.svg";
+import user from "../../../public/user.svg";
+import media from "../../../public/media.svg";
+import profile from "../../../public/profile.svg";
+import logout from "../../../public/logout.svg";
+
+const Sidebar = () => {
+  const [openedSidebar, { open, close, toggle }] = useDisclosure(false);
+
+  return (
+    <>
+      <div
+        className={`sticky h-screen ${
+          openedSidebar && "sidebar-close"
+        } bg-[#161618] border-[#3F4245] text-[#DFDFDF] select-none w-[300px] transition-all duration-500 max-xl:hidden sidebar top-0 font-nunito`}
+      >
+        <div>
+          <div className="sticky top-0 z-50 flex items-center gap-5 h-[56px] px-6 border-b-[1px] bg-[#202124] border-[#3F4245]">
+            <div
+              onClick={toggle}
+              className=" relative sidebar-menu text-3xl cursor-pointer "
+            >
+              <IoIosMenu />
+            </div>
+
+            <div className=" w-[117px] logo-container">
+              <NavLink to={`/`} className="focus-visible:outline-none">
+                <img className="" src={""} alt="" />
+              </NavLink>
+            </div>
+          </div>
+
+          <ScrollArea h={670} scrollbarSize={8} scrollHideDelay={500}>
+            <div className=" w-[300px] sidebar-body">
+              <ul className="py-8 px-6 space-y-5 list-group">
+                <li className="list-item">
+                  <NavLink
+                    to={`/`}
+                    className="side-menu "
+                    onClick={() => {
+                      close();
+                    }}
+                  >
+                    <div className="flex items-center gap-3 text-2xl hover:text-white">
+                      <img src={overview} alt="" />
+                      <span className=" list-text text-[15px] font-[600]">
+                        Overview
+                      </span>
+                    </div>
+                  </NavLink>
+                </li>
+
+                <li className="list-item">
+                  <Accordion className="">
+                    <Accordion.Item value="photos">
+                      <Accordion.Control>
+                        {/* <NavLink to={'/'} className="side-menu"> */}
+                        <div
+                          className={` side-menu flex items-center gap-3 text-2xl text-[#DFDFDF]  hover:text-white`}
+                        >
+                          <img src={sale} alt="" />
+                          <span className=" list-text text-[15px] font-[500]">
+                            Sale
+                          </span>
+                        </div>
+                        {/* </NavLink> */}
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <ul className=" text-[#DFDFDF] booking-list space-y-3">
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/booking`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Cashier</span>
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/booking-add`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Recent</span>
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  </Accordion>
+                </li>
+
+                <li className="list-item">
+                  <Accordion className="">
+                    <Accordion.Item value="photos">
+                      <Accordion.Control>
+                        {/* <NavLink to={'/'} className="side-menu"> */}
+                        <div
+                          className={` side-menu flex items-center gap-3 text-2xl text-[#DFDFDF]  hover:text-white`}
+                        >
+                          <img src={iventory} alt="" />
+                          <span className=" list-text text-[15px] font-[500]">
+                            Inventory
+                          </span>
+                        </div>
+                        {/* </NavLink> */}
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <ul className=" text-[#DFDFDF] booking-list space-y-3">
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/rooms`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Products</span>
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/room-add`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Add Products</span>
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/room-add`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Stocks Control</span>
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/room-add`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Manage Brands</span>
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  </Accordion>
+                </li>
+
+                <li className="list-item">
+                  <Accordion className="">
+                    <Accordion.Item value="photos">
+                      <Accordion.Control>
+                        {/* <NavLink to={'/'} className="side-menu"> */}
+                        <div
+                          className={` side-menu flex items-center gap-3 text-2xl text-[#DFDFDF]  hover:text-white`}
+                        >
+                          <img src={user} alt="" />
+                          <span className=" list-text text-[15px] font-[500]">
+                            User
+                          </span>
+                        </div>
+                        {/* </NavLink> */}
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <ul className=" text-[#DFDFDF] booking-list space-y-3">
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/user-overview`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Overview</span>
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/user-create`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Create User</span>
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  </Accordion>
+                </li>
+
+                <li className="list-item">
+                  <NavLink
+                    to={`/media`}
+                    className="side-menu"
+                    onClick={() => {
+                      close();
+                    }}
+                  >
+                    <div className=" flex items-center gap-3 text-2xl  hover:text-white">
+                      <img src={media} alt="" />
+                      <span className="list-text text-[15px] font-[700]">
+                        Media
+                      </span>
+                    </div>
+                  </NavLink>
+                </li>
+
+                <li className="list-item">
+                  <Accordion className="">
+                    <Accordion.Item value="photos">
+                      <Accordion.Control>
+                        {/* <NavLink to={'/'} className="side-menu"> */}
+                        <div
+                          className={` side-menu flex items-center gap-3 text-2xl text-[#DFDFDF]  hover:text-white`}
+                        >
+                          <img src={profile} alt="" />
+                          <span className=" list-text text-[15px] font-[500]">
+                            Profile
+                          </span>
+                        </div>
+                        {/* </NavLink> */}
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <ul className=" text-[#DFDFDF] booking-list space-y-3">
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/profile`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">My Account</span>
+                            </NavLink>
+                          </li>
+                          <li>
+                            <NavLink
+                              onClick={() => {
+                                close();
+                              }}
+                              to={`/profile-edit`}
+                              className="side-menu hover:text-white text-[14px]"
+                            >
+                              <span className="">Edit</span>
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  </Accordion>
+                </li>
+
+                <li className="list-item">
+                  <NavLink
+                    to={`/setting/general`}
+                    className="side-menu"
+                    onClick={() => {
+                      close();
+                    }}
+                  >
+                    <div className=" flex items-center gap-3 text-2xl  hover:text-white">
+                      <img src={logout} alt="" />
+                      <span className="list-text text-[15px] font-[700]">
+                        Logout
+                      </span>
+                    </div>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </ScrollArea>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,144 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import clock from "../../public/Clock.svg";
+import envelope from "../../public/EnvelopeOpen.svg";
+import phoneCall from "../../public/PhoneCall.svg";
+import saleColor from "../../public/saleColor.svg";
+import { Tabs } from "@mantine/core";
+import "./profile.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className=" mx-6 my-3">
+      <div className="flex justify-between items-center mb-20">
+        <div>
+          <h3 className=" text-[22px] font-[600] text-white mb-[8px]">
+            Profile
+          </h3>
+          <p className=" text-[14px] font-[500] tracking-[0.15px] text-white/70">
+            Profile/ My Account/ Information
+          </p>
+        </div>
+        <div className="">
+          <div className="px-[16px] py-[7px] flex items-center gap-2 text-[#202124] font-[600]  bg-[#8AB4F8] rounded-[5px]">
+            <AiOutlinePlus size={16} />
+            Edit Profile
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[#1A1A1A] text-white ">
+        <div className=" flex justify-between items-end pl-16 pr-8">
+          <div className=" flex items-end gap-[33px] mt-[-50px]">
+            <div className="">
+              <img
+                className=" w-[139px] h-[139px] object-cover rounded-full"
+                src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVvcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                alt=""
+              />
+            </div>
+            <div>
+              <h3 className=" text-[26px] font-[600]">Ethan James</h3>
+              <p className=" text-[14px] font-[500] text-[#C5C1C1] flex items-center gap-1">
+                Sale Executive /{" "}
+                <span className="flex  gap-1 text-[10px] font-[500] text-[#C5C1C1]">
+                  <img src={clock} alt="" />
+                  Active in 1hr
+                </span>
+              </p>
+            </div>
+          </div>
+
+          <div className=" flex items-center gap-[10px]">
+            <div className=" w-[33px] h-[33px] rounded-full bg-[#202124] flex items-center justify-center">
+              <img src={envelope} alt="" />
+            </div>
+            <div className=" w-[33px] h-[33px] rounded-full bg-[#202124] flex items-center justify-center">
+              <img src={phoneCall} alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className=" mt-10">
+          <Tabs color="gray" radius="xs" defaultValue="gallery">
+            <Tabs.List>
+              <Tabs.Tab value="gallery" icon={<img src={saleColor} />}>
+                Personal
+              </Tabs.Tab>
+              <Tabs.Tab value="messages" icon={<img src={saleColor} />}>
+                Login information
+              </Tabs.Tab>
+              <Tabs.Tab value="settings" icon={<img src={saleColor} />}>
+                Password
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="gallery" pt="xs">
+              <div className="pl-16 pr-8 flex flex-col gap-[30px] py-[30px]">
+                <div className=" flex items-center gap-[118px]">
+                  <p className=" text-[#B9B9B9] text-[18px] font-[600]">
+                    Date of Birth
+                  </p>
+                  <p className=" text-[18px] font-[500] leading-[21.94px]">
+                    : 12/4/1999
+                  </p>
+                </div>
+                <div className=" flex items-center gap-[164px]">
+                  <p className=" text-[#B9B9B9] text-[18px] font-[600]">
+                    Gender
+                  </p>
+                  <p className=" text-[18px] font-[500] leading-[21.94px]">
+                    : Male
+                  </p>
+                </div>
+                <div className=" flex items-center gap-[159px]">
+                  <p className=" text-[#B9B9B9] text-[18px] font-[600]">
+                    Address
+                  </p>
+                  <p className=" text-[18px] font-[500] leading-[21.94px]">
+                    : No2 ,Baho Street ,Bahan ,Yagon.
+                  </p>
+                </div>
+              </div>
+            </Tabs.Panel>
+
+            <Tabs.Panel value="messages" pt="xs">
+              <div className="pl-16 pr-8 flex flex-col gap-[30px] py-[30px]">
+                <div className=" flex items-center gap-[140px]">
+                  <p className=" text-[#B9B9B9] text-[18px] font-[600]">
+                    Phone
+                  </p>
+                  <p className=" text-[18px] font-[500] leading-[21.94px]">
+                    : 0985678787
+                  </p>
+                </div>
+                <div className=" flex items-center gap-[127px]">
+                  <p className=" text-[#B9B9B9] text-[18px] font-[600]">
+                    Position
+                  </p>
+                  <p className=" text-[18px] font-[500] leading-[21.94px]">
+                    : Sale Executive
+                  </p>
+                </div>
+                <div className=" flex items-center gap-[158px]">
+                  <p className=" text-[#B9B9B9] text-[18px] font-[600]">
+                    Mail
+                  </p>
+                  <p className=" text-[18px] font-[500] leading-[21.94px]">
+                    : ethan123@gmail.com
+                  </p>
+                </div>
+              </div>
+            </Tabs.Panel>
+
+            <Tabs.Panel value="settings" pt="xs">
+              Password tab content
+            </Tabs.Panel>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
