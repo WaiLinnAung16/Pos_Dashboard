aac994b6cbf1d3a812c9db06004589fa9db1c686
WaiLinnAung16 <waia00198@gmail.com>
2023-08-17 15:14:36 +0630

    add product

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,84 +1,59 @@
-import { Table } from "@mantine/core";
 import React from "react";
 import { BsArrowRightShort } from "react-icons/bs";
 import { MdEdit } from "react-icons/md";
 import { FaMinus } from "react-icons/fa";
 const UserList = () => {
-  const elements = [
+  const rows = [
     {
-      num: 1,
+      id: 1,
       name: "Wai Linn Aung",
       position: "Staff",
-      email: "waia@gmail.com",
+      email: "email.com",
     },
     {
-      num: 2,
+      id: 2,
       name: "Wai Linn Aung",
       position: "Staff",
-      email: "waia@gmail.com",
-    },
-    {
-      num: 3,
-      name: "Wai Linn Aung",
-      position: "Staff",
-      email: "waia@gmail.com",
-    },
-    {
-      num: 4,
-      name: "Wai Linn Aung",
-      position: "Staff",
-      email: "waia@gmail.com",
-    },
-    {
-      num: 5,
-      name: "Wai Linn Aung",
-      position: "Staff",
-      email: "waia@gmail.com",
+      email: "email.com",
     },
   ];
-  const rows = elements.map((element) => (
-    <tr key={element.num} className="text-white">
-      <td>{element.num}</td>
-      <td>{element.name}</td>
-      <td>{element.position}</td>
-      <td>{element.email}</td>
-      <td>
-        <div className="flex items-center gap-3">
-          <div className=" w-8 h-8 bg-[#3F4245] grid place-content-center rounded-full">
-            <FaMinus className="text-lg" />
-          </div>
-          <div className=" w-8 h-8 bg-[#3F4245] grid place-content-center rounded-full">
-            <MdEdit className="text-lg" />
-          </div>
-          <div className=" w-8 h-8 bg-[#3F4245] grid place-content-center rounded-full">
-            <BsArrowRightShort className="text-lg" />
-          </div>
-        </div>
-      </td>
-    </tr>
-  ));
-
   return (
-    <Table withBorder className="mt-5 ">
+    <table className=" table-fixed  text-white w-full mt-5">
       <thead>
-        <tr>
-          <th>
-            <span className="text-white">No.</span>
-          </th>
-          <th>
-            <span className="text-white">Name</span>
-          </th>
-          <th>
-            <span className="text-white">Position</span>
-          </th>
-          <th>
-            <span className=" text-white">Email</span>
-          </th>
-          <th className="w-[200px]"></th>
+        <tr className=" border border-[#3F4245]">
+          <th className="   py-2 px-5 text-start">No</th>
+          <th className="  py-2 px-5 text-start">Name</th>
+          <th className="  py-2 px-5 text-start">Position</th>
+          <th className="  py-2 px-5 text-start">Email</th>
+          <th className="  py-2 px-5 text-start"></th>
         </tr>
       </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+      <tbody>
+        {rows.map((row) => {
+          return (
+            <tr key={row.id} className="border border-[#3F4245]">
+              <td className=" py-2 px-5">{row.id}</td>
+              <td className=" py-2 px-5">{row.name}</td>
+              <td className=" py-2 px-5">{row.position}</td>
+              <td className=" py-2 px-5">{row.position}</td>
+              <td className=" py-2 px-5">
+                <div className="flex items-center gap-3">
+                  <div className=" w-8 h-8 bg-[#3F4245] grid place-content-center rounded-full">
+                    <FaMinus className="text-lg" />
+                  </div>
+                  <div className=" w-8 h-8 bg-[#3F4245] grid place-content-center rounded-full">
+                    <MdEdit className="text-lg" />
+                  </div>
+                  <div className=" w-8 h-8 bg-[#3F4245] grid place-content-center rounded-full">
+                    <BsArrowRightShort className="text-lg" />
+                  </div>
+                </div>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
